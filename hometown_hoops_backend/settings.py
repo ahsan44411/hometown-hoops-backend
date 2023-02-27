70996b4dc0d5e72ad2120abb401c7db48826222a
ahsan <ahsan44411@gmail.com>
2023-02-27 19:33:46 +0500

    requirements

--- hometown_hoops_backend/settings.py
+++ hometown_hoops_backend/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "accounts/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
